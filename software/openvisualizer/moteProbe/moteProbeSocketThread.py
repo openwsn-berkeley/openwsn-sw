c8b6d71afdc78b7cea6a885107610c5a09fb9103
Thomas Watteyne <watteyne@eecs.berkeley.edu>
2012-06-24 06:20:53 +0000

    comments. dummy test.

--- software/openvisualizer/moteProbe/moteProbeSocketThread.py
+++ software/openvisualizer/moteProbe/moteProbeSocketThread.py
@@ -10,7 +10,7 @@ class moteProbeSocketThread(threading.Thread):
         
         # local variables
         self.socket          = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.conn            =  None
+        self.conn            = None
         
         # initialize the parent class
         threading.Thread.__init__(self)
@@ -19,19 +19,28 @@ class moteProbeSocketThread(threading.Thread):
         self.name            = 'moteProbeSocketThread@'+str(self.socketport)
     
     def run(self):
-        self.socket.bind(('',self.socketport)) # attach to a socket on whatever IPv4 address of the computer
-        self.socket.listen(1)                  # listen for incoming connection requests from the OpenVisualizer
+        
+        # attach to a socket on all interfaces of the computer
+        self.socket.bind(('',self.socketport))
+        
+        # listen for incoming connection requests
+        self.socket.listen(1)
+        
         while True:
             # wait for OpenVisualizer to connect
             self.conn,self.addr = self.socket.accept()
-            # record that I'm connected now
+            
+            # TODO log
             print datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S")+': openVisualizer connection from '+str(self.addr)
+            
             # read data sent from OpenVisualizer
             while True:
                 try:
                     bytesReceived = self.conn.recv(4096)
                     self.otherThreadHandler.send(bytesReceived)
                 except socket.error:
+                
+                    # TODO log
                     print datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S")+': openVisualizer disconnected'
                     self.conn = None
                     break
