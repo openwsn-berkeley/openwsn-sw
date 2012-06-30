ddc7744dd1dbf4db42f7dbb1603b1797662d8cb8
Thomas Watteyne <watteyne@eecs.berkeley.edu>
2012-06-30 23:44:57 +0000

    adding CLI to moteProbe

--- software/openvisualizer/moteProbe/moteProbe.py
+++ software/openvisualizer/moteProbe/moteProbe.py
@@ -55,14 +55,34 @@ it has to create moteProbe threads for each mote connected
 '''
 if __name__ == '__main__':
     
-    import logging.handlers
+    import os
+    import sys
+    cur_path = sys.path[0]
+    sys.path.insert(0, os.path.join(cur_path, '..', '..', 'openCli'))      # openCli/
+    
+    import       logging.handlers
+    from OpenCli import OpenCli
+    
+    class moteProbeCli(OpenCli):
+    
+        def __init__(self,moteProbe_handlers):
+            
+            # store params
+            self.moteProbe_handlers     = moteProbe_handlers
+            
+            # initialize parent class
+            OpenCli.__init__(self,"mote probe CLI",self.quit_cb)
+        
+        def quit_cb(self):
+            
+            for mb in self.moteProbe_handlers:
+               mb.quit()
     
     # logging
     logHandler = logging.handlers.RotatingFileHandler('moteProbe.log',
-                                                   maxBytes=2000000,
-                                                   backupCount=5,
-                                                   mode='w'
-                                                   )
+                                                      maxBytes=2000000,
+                                                      backupCount=5,
+                                                      mode='w')
     logHandler.setFormatter(logging.Formatter("%(asctime)s [%(name)s:%(levelname)s] %(message)s"))
     for loggerName in ['moteProbe',
                        'moteProbeSerialThread',
@@ -72,13 +92,14 @@ if __name__ == '__main__':
         temp.setLevel(logging.DEBUG)
         temp.addHandler(logHandler)
     
-    # banner
-    banner = 'moteProbe - Open WSN project'
-    print banner
-    log.info(banner)
+    moteProbe_handlers = []
     
     # create and start mote probe instances
     serialPortNames     = utils.findSerialPorts()
     port_numbers        = [8080+i for i in range(len(serialPortNames))]
     for (serialPortName,port_number) in zip(serialPortNames,port_numbers):
-        moteProbe(serialPortName,port_number)
+        moteProbe_handlers.append(moteProbe(serialPortName,port_number))
+    
+    # cli
+    cli = moteProbeCli(moteProbe_handlers)
+    cli.start()
