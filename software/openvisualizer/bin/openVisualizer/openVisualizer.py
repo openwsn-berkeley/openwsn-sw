478da29169c4c1aa279ee1193d31af992f0670bc
Thomas Watteyne <watteyne@eecs.berkeley.edu>
2012-06-30 23:11:26 +0000

    cleaner module initialization

--- software/openvisualizer/bin/openVisualizer/openVisualizer.py
+++ software/openvisualizer/bin/openVisualizer/openVisualizer.py
@@ -1,16 +1,12 @@
 import sys
 import os
-sys.path.insert(0, os.path.join(sys.path[0], '..', '..'))
-
-import logging
-import logging.handlers
+if __name__=='__main__':
+    cur_path = sys.path[0]
+    sys.path.insert(0, os.path.join(cur_path, '..', '..')) # openvisualizer/
 
 from moteProbe     import moteProbe
 from moteConnector import moteConnector
 from moteState     import moteState
-#from lbrClient     import lbrClient
-#from processing    import openRecord
-#from openUI        import openDisplay
 
 LOCAL_ADDRESS  = '127.0.0.1'
 TCP_PORT_START = 8090
@@ -21,49 +17,31 @@ def main():
     moteConnector_handlers = []
     moteState_handlers     = []
     
-    #===== moteProbe
-    
+    # create a moteProbe for each mote connected to this computer
     serialPorts    = moteProbe.utils.findSerialPorts()
     tcpPorts       = [TCP_PORT_START+i for i in range(len(serialPorts))]
     for (serialPort,tcpPort) in zip(serialPorts,tcpPorts):
         moteProbe_handlers.append(moteProbe.moteProbe(serialPort,tcpPort))
     
-    #===== moteConnector
+    # create a moteConnector for each moteProbe
+    for mp in moteProbe_handlers:
+       moteConnector_handlers.append(moteConnector.moteConnector(LOCAL_ADDRESS,mp.getTcpPort()))
     
-    for temp_moteProbe in moteProbe_handlers:
-       moteConnector_handlers.append(moteConnector.moteConnector(LOCAL_ADDRESS,temp_moteProbe.getTcpPort()))
-    
-    #===== moteState
-    
-    for temp_moteConnector in moteConnector_handlers:
-       moteState_handlers.append(moteState.moteState(temp_moteConnector))
-    
-    '''
-    # create a recordElement and a displayElement for each motePortNetworkThread
-    for key,value in shared.moteConnectors.iteritems():
-       openRecord.createRecordElement(key)
-       openDisplay.createDisplayElement(key)
-
-    # create an lbrClientThread
-    shared.lbrClientThread = lbrClient.lbrClientThread(shared.lbrFrame,
-                                                       openDisplay.tkSemaphore)
-    '''
+    # create a moteState for each moteConnector
+    for mc in moteConnector_handlers:
+       moteState_handlers.append(moteState.moteState(mc))
     
     # start threads
-    for temp_moteConnector in moteConnector_handlers:
-       temp_moteConnector.start()
-    #shared.lbrClientThread.start()
-    #openDisplay.startGUI()
-
-#============================ logging =========================================
-
-#============================ logging =========================================
-
+    for mc in moteConnector_handlers:
+       mc.start()
+    
+#============================ application logging =============================
+import logging
+import logging.handlers
 logHandler = logging.handlers.RotatingFileHandler('openVisualizer.log',
-                                               maxBytes=2000000,
-                                               backupCount=5,
-                                               mode='w'
-                                               )
+                                                  maxBytes=2000000,
+                                                  backupCount=5,
+                                                  mode='w')
 logHandler.setFormatter(logging.Formatter("%(asctime)s [%(name)s:%(levelname)s] %(message)s"))
 for loggerName in ['moteProbe',
                    'moteConnector',
