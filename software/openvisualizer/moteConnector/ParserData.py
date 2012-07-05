53c1f09f337681957ea8401b0bb87432f38ef2fd
Thomas Watteyne <watteyne@eecs.berkeley.edu>
2012-07-05 03:46:24 +0000

    removing CSV'ing and printing of data

--- software/openvisualizer/moteConnector/ParserData.py
+++ software/openvisualizer/moteConnector/ParserData.py
@@ -10,19 +10,12 @@ log.addHandler(NullHandler())
 from ParserException import ParserException
 import Parser
 
-import csv
-
 class ParserData(Parser.Parser):
-    
-    CSV_FILENAME = 'rawdata.csv'
     
     def __init__(self):
         
         # log
         log.debug("create instance")
-        
-        # create a csv writer
-        self.csvWriter = csv.writer(open(self.CSV_FILENAME, 'wb'), delimiter=',')
         
         # initialize parent class
         Parser.Parser.__init__(self,1)
@@ -31,13 +24,8 @@ class ParserData(Parser.Parser):
     
     def parseInput(self,input):
         
-        print input
-        
         # log
         log.debug("received data {0}".format(input))
-        
-        # write csv
-        self.csvWriter.writerow(input)
         
         raise ParserException(ParserException.NOT_IMPLEMENTED)
     
