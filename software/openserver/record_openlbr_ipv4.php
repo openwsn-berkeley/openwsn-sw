ef493f9535d48fb72bbc915bd6a3873aba4ae1d6
Thomas Watteyne <watteyne@eecs.berkeley.edu>
2012-07-08 22:27:05 +0000

    moving files around

--- software/openserver/record_openlbr_ipv4.php
+++
@@ -1,9 +0,0 @@
-<?php
-$file_name = "ipv4_".$_GET["hostname"].".txt";
-$file_handler = fopen($file_name, "w");
-$logdetails = $_SERVER['REMOTE_ADDR'];
-fwrite($file_handler, $logdetails);
-fclose($file_handler);
-?>
-
-<html><body>This should be accessed only by an LBR.</body></html>
