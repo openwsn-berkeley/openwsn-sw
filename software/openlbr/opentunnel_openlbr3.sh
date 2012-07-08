c4b5bada5b066c598f02e8d2b8e10a70fdac6731
Thomas Watteyne <watteyne@eecs.berkeley.edu>
2012-07-08 22:22:59 +0000

    moving files around

--- software/openlbr/opentunnel_openlbr3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#set up opentunnel_3
-modprobe ipv6
-ip tunnel add opentunnel_3 mode sit remote 72.52.104.74 local `cat my_openipv4.txt` ttl 255
-ip link set opentunnel_3 up
-ip addr add 2001:470:1f04:1195::2/64 dev opentunnel_3
-ip route add ::/0 dev opentunnel_3
-ip -f inet6 addr
--- software/openlbr/report_address.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#================================================ report address
-
-wget -O - http://wsn.eecs.berkeley.edu/openserver/record_openlbr_ipv4.php?hostname=`cat /etc/hostname`
-
