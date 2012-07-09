a9251a4bcc4f384d3987f43c827b3f37cb4de1ce
Thomas Watteyne <watteyne@eecs.berkeley.edu>
2012-07-09 02:50:00 +0000

    cleaner timestamp formatting when printing

--- software/openEndPoint/publisher/PublisherScreen.py
+++ software/openEndPoint/publisher/PublisherScreen.py
@@ -6,13 +6,22 @@ log = logging.getLogger('PublisherScreen')
 log.setLevel(logging.ERROR)
 log.addHandler(NullHandler())
 
+import time
+
 import Publisher
 
 class PublisherScreen(Publisher.Publisher):
     
+    FORMAT_TIMESTAMP = '%Y/%m/%d %H:%M:%S'
+    
     #======================== public ==========================================
     
     def publish(self,timestamp,source,data):
-        print "{2} from {1} at {0}".format(timestamp,source,data)
+        print "{2} from {1} at {0}".format(self._formatTimestamp(timestamp),
+                                           source,
+                                           data)
     
     #======================== private =========================================
+    
+    def _formatTimestamp(self,timestamp):
+        return time.strftime(self.FORMAT_TIMESTAMP,time.localtime(timestamp))
