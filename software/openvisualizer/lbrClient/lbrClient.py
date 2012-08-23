41e4a6f32e3970e3607c717535074526d2e0024a
Thomas Watteyne <watteyne@eecs.berkeley.edu>
2012-08-23 04:56:42 +0000

    removing RPL from lbrClient; added to networkState (dummy)

--- software/openvisualizer/lbrClient/lbrClient.py
+++ software/openvisualizer/lbrClient/lbrClient.py
@@ -10,7 +10,6 @@ import threading
 from moteConnector import MoteConnectorConsumer
 import socket
 import copy
-import RPL
 
 class lbrClientMoteConnectorConsumer(MoteConnectorConsumer.MoteConnectorConsumer):
     
@@ -42,9 +41,7 @@ class lbrClient(threading.Thread):
     
         # store params
         self.moteConnector   = moteConnector
-        self.RPL             = RPL.RPL() #RPL source routing table
         
-        #self.RPL.test()
         # log
         log.debug("creating instance")
         
@@ -54,9 +51,6 @@ class lbrClient(threading.Thread):
         self.connectSem           = threading.Lock()
         self.connectorConsumer    = lbrClientMoteConnectorConsumer(self.moteConnector,
                                                                    self.send,self.moteConnector.TYPE_DATA_INTERNET)
-        #for RPL data local consumer that updates RPL information
-        self.connectorConsumerLocal  = lbrClientMoteConnectorConsumer(self.moteConnector,
-                                                                   self.handleLocalData,self.moteConnector.TYPE_DATA_LOCAL)
         
         # reset the statistics
         self._resetStats()
@@ -77,7 +71,6 @@ class lbrClient(threading.Thread):
         
         #start the moteConnectorConsumer
         self.connectorConsumer.start()
-        self.connectorConsumerLocal.start()
         
         while True:
             # reset the statistics
@@ -265,15 +258,6 @@ class lbrClient(threading.Thread):
         except socket.error as err:
             log.error('socket error while sending: {0}'.format(err))
     
-    #this method handles RPL data, when invoked a DAO is passed as parameter. This information needs to be used to update
-    #RPL routing tables at RPL component. The method is invoked by Parser as this function is the callback passed to
-    #lbrClientMoteConnectorConsumer with filter TYPE_DATA_LOCAL
-    def handleLocalData(self,rplData):
-        self.RPL.update(rplData)
-        print "handleLocalData!"
-        log.debug('rplDATA update: {0}'.format(rplData))
-        
-                
     def getStats(self):
         self.statsLock.acquire()
         returnVal = copy.deepcopy(self.stats)
