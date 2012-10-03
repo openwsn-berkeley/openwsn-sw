1a017600755071c7cf752ecdaeefca1e2b75993b
Xavier Vilajosana <xvilajosana@eecs.berkeley.edu>
2012-10-03 23:59:44 +0000

    filtering short packets

--- software/openvisualizer/moteConnector/ParserData.py
+++ software/openvisualizer/moteConnector/ParserData.py
@@ -45,39 +45,42 @@ class ParserData(Parser.Parser):
         log.debug("destination address of the packet is {0} ".format(a))
         a="".join(hex(c) for c in source)
         log.debug("source address of the packet is {0} ".format(a))
-        
-        iphcHeader  = input[18:20]
-        
-        #from 6LoWPAN compression draft:
-        # DAM/SAM 
-        # 0 bits.  The address is fully elided.  The first 64 bits
-        #    of the address are the link-local prefix padded with zeros.
-        #    The remaining 64 bits are computed from the encapsulating
-        #    header (e.g. 802.15.4 or IPv6 destination address)
-        
-        sam  = (iphcHeader[1] >> self.IPHC_SAM) & 0x03 #2b
-        dam  = (iphcHeader[1] >> self.IPHC_DAM) & 0x03 #2b
-        
-        if (sam==0x01 and dam==0x03):
-            #source is not elided so it is in the iphc header.
-            #skip 2 bytes of ICMP header being nexhop, hop limit,..
-            icmpHeader = input[20:22]
-            source=input[22:30]
-            rplheader=input[30:32]
-
-            if (rplheader[0]==155 and rplheader[1]==4):
-                #this is a DAO
-                eventType = 'data.local'
-                #keep src and dest for local data 
-                input = input[2:]
-                log.debug("data is local")  
-                
-                return (eventType,input)
+       
+       
+        if (len(input) > 20):
+            iphcHeader  = input[18:20]
+            
+            #from 6LoWPAN compression draft:
+            # DAM/SAM 
+            # 0 bits.  The address is fully elided.  The first 64 bits
+            #    of the address are the link-local prefix padded with zeros.
+            #    The remaining 64 bits are computed from the encapsulating
+            #    header (e.g. 802.15.4 or IPv6 destination address)
+            
+            sam  = (iphcHeader[1] >> self.IPHC_SAM) & 0x03 #2b
+            dam  = (iphcHeader[1] >> self.IPHC_DAM) & 0x03 #2b
+            
+            if (sam==0x01 and dam==0x03):
+                #source is not elided so it is in the iphc header.
+                #skip 2 bytes of ICMP header being nexhop, hop limit,..
+                icmpHeader = input[20:22]
+                source=input[22:30]
+                rplheader=input[30:32]
+    
+                if (rplheader[0]==155 and rplheader[1]==4):
+                    #this is a DAO
+                    eventType = 'data.local'
+                    #keep src and dest for local data 
+                    input = input[2:]
+                    log.debug("data is local")  
+                    
+                    return (eventType,input)
+                else:
+                    pass
             else:
                 pass
         else:
             pass
-        
         
         #No DAO, it is data internet.
         eventType = 'data.internet'
