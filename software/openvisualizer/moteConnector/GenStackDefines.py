8d3ee756230493c82e41d81aa2c423ebc2ccd931
Thomas Watteyne <twatteyne@gmail.com>
2013-01-19 13:26:17 -0800

    Updating the GenStackDefines.py script so it works with the new directory organization.

--- software/openvisualizer/moteConnector/GenStackDefines.py
+++ software/openvisualizer/moteConnector/GenStackDefines.py
@@ -1,6 +1,15 @@
 ##
-# Standalone script to generate the StackDefines.py file
-# Double click to regenerate the file; assumes firmware/ lives next to software/
+# Standalone script to generate the StackDefines.py file.
+#
+# This script extracts all the information it needs from the openwsn.h
+# header file (part of the openwsn-fw repository), and generate the
+# StackDefines.py file (part of the openwsn-sw repository).
+#
+# To run it, just double-click on this file.
+#
+# \note To have to check out the openwsn-fw and openwsn-sw side-by-side, i.e.
+#       you should have a directory with two subdirectories openwsn-fw/ and 
+#       openwsn-sw/ somewhere on your computer.
 #
 # \author Thomas Watteyne <watteyne@eecs.berkeley.edu>, August 2010
 #
@@ -9,14 +18,18 @@ import os
 import re
 import time
 
-OUTPUT_FILE         = 'StackDefines.py'
-LOCATION_OPENWSN_H  = os.path.join('..','..','..','software','..','firmware','openos','openwsn')
+#============================ defines =========================================
+
+INPUT_FILE    = os.path.join('..','..','..','..','openwsn-fw','firmware','openos','openwsn','openwsn.h')
+OUTPUT_FILE   = 'StackDefines.py'
+
+#============================ helpers =========================================
 
 def genComponentCodes():
     
     # find components code in openwsn.h
     codesFound = []
-    for line in open(os.path.join(LOCATION_OPENWSN_H,'openwsn.h')):
+    for line in open(INPUT_FILE,'r'):
         m = re.search('\s*COMPONENT_(\S*)\s*=\s*(\S*),\s*',line)
         if m:
             name = m.group(1)
@@ -39,7 +52,7 @@ def genErrorDescriptions():
     
     # find components code in openwsn.h
     codesFound = []
-    for line in open(os.path.join(LOCATION_OPENWSN_H,'openwsn.h')):
+    for line in open(INPUT_FILE,'r'):
         m = re.search('\s*ERR_(\S*)\s*=\s*(\S*),\s*\/\/\s*([\S\s]*)',line)
         if m:
             name = m.group(1)
@@ -59,26 +72,47 @@ def genErrorDescriptions():
     
     return output
 
-def main():
+#============================ main ============================================
 
-    # gather the information
-    output  = []
-    output += ["# DO NOT EDIT DIRECTLY!"]
-    output += ["# This file was generated automatically by GenStackDefines.py"]
-    output += ["# on {0}".format(time.strftime("%a, %d %b %Y %H:%M:%S"))]
-    output += ["#"]
-    output += [""]
-    output += [genComponentCodes()]
-    output += [""]
-    output += [genErrorDescriptions()]
-    output += [""]
+def main():
     
-    # write to file
-    file = open(OUTPUT_FILE,'w')
-    file.write('\n'.join(output))
-    file.close
-    
-    raw_input('Script ended normally. Press enter to close.')
+    if os.path.exists(INPUT_FILE):
+        # we can access the openwsn.h file
+        
+        # gather the information
+        output  = []
+        output += ["# DO NOT EDIT DIRECTLY!"]
+        output += ["# This file was generated automatically by GenStackDefines.py"]
+        output += ["# on {0}".format(time.strftime("%a, %d %b %Y %H:%M:%S"))]
+        output += ["#"]
+        output += [""]
+        output += [genComponentCodes()]
+        output += [""]
+        output += [genErrorDescriptions()]
+        output += [""]
+        output  = '\n'.join(output)
+        
+        # write to file
+        file = open(OUTPUT_FILE,'w')
+        file.write(output)
+        file.close
+        
+        print "{0} created successfully.".format(OUTPUT_FILE)
+        
+    else:
+        # we can NOT access the openwsn.h file
+        
+        # print error message
+        output  = []
+        output += ["ERROR: could not open the following file"]
+        output += ["   {0}".format(INPUT_FILE)]
+        output += [""]
+        output += ["Do you have the openwsn-fw and openwsn-sw repositories"]
+        output += ["checked out side-by-side?"]
+        output  = '\n'.join(output)
+        print output
+        
+    raw_input('\nScript ended. Press enter to close.')
 
 if __name__ == '__main__':
     main()
