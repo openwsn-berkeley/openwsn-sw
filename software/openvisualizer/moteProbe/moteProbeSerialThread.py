1bd2806872a0045b19cbb263eaf1b84426088281
Thomas Watteyne <twatteyne@gmail.com>
2013-01-27 14:59:55 -0800

    change to moteProbe/moteProbeSerialThread.py to use HDLC framing.

--- software/openvisualizer/moteProbe/moteProbeSerialThread.py
+++ software/openvisualizer/moteProbe/moteProbeSerialThread.py
@@ -11,6 +11,7 @@ import threading
 import serial
 import time
 import struct
+import openhdlc
 
 from pydispatch import dispatcher
 
@@ -31,6 +32,7 @@ class moteProbeSerialThread(threading.Thread):
         self.serialOutputLock     = threading.Lock()
         self.state                = 'WAIT_HEADER'
         self.numdelimiter         = 0
+        self.hdlc                 = openhdlc.OpenHdlc()
         
         # initialize the parent class
         threading.Thread.__init__(self)
@@ -50,7 +52,7 @@ class moteProbeSerialThread(threading.Thread):
         log.debug("start running")
     
         while True:     # open serial port
-            log.debug("open serial port")
+            log.debug("open serial port {0}@{1}".format(self.serialportName,self.serialportBaudrate))
             self.serial = serial.Serial(self.serialportName,self.serialportBaudrate)
             while True: # read bytes from serial port
                 try:
@@ -61,31 +63,32 @@ class moteProbeSerialThread(threading.Thread):
                     break
                 else:
                     if    self.state == 'WAIT_HEADER':
-                        if char == '^':
+                        if char == self.hdlc.HDLC_FLAG:
                             self.numdelimiter     += 1
                         else:
                             self.numdelimiter      = 0
-                        if self.numdelimiter==3:
+                        if self.numdelimiter==1:
                             self.state             = 'RECEIVING_COMMAND'
-                            self.serialInput       = ''
+                            self.serialInput       = char
                             self.numdelimiter      = 0
                     elif self.state == 'RECEIVING_COMMAND':
                         self.serialInput = self.serialInput+char
-                        if char == '$':
+                        if char == self.hdlc.HDLC_FLAG:
                             self.numdelimiter     += 1
                         else:
                             self.numdelimiter      = 0
-                        if self.numdelimiter==3:
+                        if self.numdelimiter==1:
                             self.state             = 'WAIT_HEADER'
                             self.numdelimiter      = 0
-                            self.serialInput       = self.serialInput.rstrip('$')
-                            #byte 0 is the type of status message
-                            if self.serialInput[0]=="R":     #request for data
-                                if (ord(self.serialInput[1])==200):  # byte 1 indicates free space in mote's input buffer
-                                    self.serialOutputLock.acquire()
-                                    self.serial.write(self.serialOutput)
-                                    self.serialOutput = ''
-                                    self.serialOutputLock.release()
+                            try:
+                                self.serialInput   = self.hdlc.dehdlcify(self.serialInput)
+                            except openhdlc.HdlcException:
+                                print "wrong CRC!"
+                            if self.serialInput==['R']:
+                                with self.serialOutputLock:
+                                    if(len(self.serialOutput)>0):
+                                        self.serial.write(self.hdlc.hdlcify(self.serialOutput))
+                                        self.serialOutput = ''
                             else:
                                 # dispatch
                                 dispatcher.send(
@@ -103,7 +106,7 @@ class moteProbeSerialThread(threading.Thread):
             log.error("serialOutput overflow ({0} bytes)".format(len(self.serialOutput)))
         
         self.serialOutput += data[0]+ chr(len(self.serialOutput)) + data[1:]
-        if len(self.serialOutput)>200:
+        if len(self.serialOutput)>251:
             log.warning("serialOutput overflowing ({0} bytes)".format(len(self.serialOutput)))
         self.serialOutputLock.release()
     
