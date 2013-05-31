f5d57f3f583b4074f933a7f0ee1528d891a6c6f8
Xavier Vilajosana <xvilajosana@eecs.berkeley.edu>
2013-05-31 15:02:13 -0700

    correcting order of params in 6tuscli app.

--- software/openApps/6tusCli/sixtusCoap.py
+++ software/openApps/6tusCli/sixtusCoap.py
@@ -45,49 +45,85 @@ class sixtusCoap(object):
         params  = []
         params += [self.CREATE_LINK_TYPE]
         params += [1] #one link
-        params += [target_addr] 
-        params += [slot_num]
-        params += [slot_type]  
-        params += [shared] 
-        params += [ch_offset]  
+        target_addr=self._convertAddress2bytes(target_addr) 
+        params += target_addr 
+        params += [int(slot_type)]  
+        params += [int(shared)] 
+        #2bytes slotnum:
+        slotnum=int(slot_num)
+        byte1=(slotnum>>8)&0x00FF
+        byte2=(slotnum)&0x00FF
+        params += [byte2]
+        params += [byte1]
+        params += [int(ch_offset)]
+          
         log.debug('Following PUT command entered:'+ ",".join( str(c) for c in params))
         
         url='coap://[{0}]:{1}/{2}/'.format(dst_url,d.DEFAULT_UDP_PORT,self.RESOURCE)
-        
-        self.coap.PUT(uri=url,payload=params)
-        
+        try:
+            self.coap.PUT(uri=url,payload=params)
+        except Exception as err:
+            log.error("Cannot access the resource {0}:{1}".format(url,err))
+            print "Cannot access the resource {0}:{1}".format(url,err)
+            
+            
+               
     def UPDATE_LINK(self,dst_url,target_addr,slot_num,slot_type,shared,ch_offset):
         params  = []
         params += [self.UPDATE_LINK_TYPE]
         params += [1] #one link
-        params += [target_addr] 
-        params += [slot_num]
-        params += [slot_type]  
-        params += [shared] 
-        params += [ch_offset]  
+        target_addr=self._convertAddress2bytes(target_addr) 
+        params += target_addr  
+        params += [int(slot_type)]  
+        params += [int(shared)] 
+        #2bytes slotnum:
+        slotnum=int(slot_num)
+        byte1=(slotnum>>8)&0x00FF
+        byte2=(slotnum)&0x00FF
+        params += [byte2]
+        params += [byte1]
+        params += [int(ch_offset)] 
         log.debug('Following UPDATE command entered:'+ ",".join( str(c) for c in params))
         
         url='coap://[{0}]:{1}/{2}/'.format(dst_url,d.DEFAULT_UDP_PORT,self.RESOURCE)
-        self.coap.PUT(uri=url,payload=params)
-       
+        try:
+            self.coap.PUT(uri=url,payload=params)
+        except Exception as err:
+            log.error("Cannot access the resource {0}:{1}".format(url,err))
+            print "Cannot access the resource {0}:{1}".format(url,err)
+            
     def READ_LINK(self,dst_url,target_addr,slot_num):
+        target_addr=self._convertAddress2bytes(target_addr) 
         url='coap://[{0}]:{1}/{2}/{3}/{4}/{5}/{6}'.format(dst_url,d.DEFAULT_UDP_PORT,self.RESOURCE,self.READ_LINK_TYPE,1,target_addr,slot_num)
         log.debug('Following GET command entered:'+ url)
         print url
-        res=self.coap.GET(uri=url)
-        return res
-    
+        try:
+           res=self.coap.GET(uri=url)
+           return res
+        except Exception as err:
+            log.error("Cannot access the resource {0}:{1}".format(url,err))
+            print "Cannot access the resource {0}:{1}".format(url,err)
+            
     def DELETE_LINK(self,dst_url,target_addr,slot_num):
         params  = []
         params += [self.DELETE_LINK_TYPE]
         params += [1] #one link
-        params += [target_addr] 
-        params += [slot_num]
+        target_addr=self._convertAddress2bytes(target_addr) 
+        params += target_addr
+        #2bytes slotnum:
+        slotnum=int(slot_num)
+        byte1=(slotnum>>8)&0x00FF
+        byte2=(slotnum)&0x00FF
+        params += [byte2]
+        params += [byte1]  
         url='coap://[{0}]:{1}/{2}/'.format(dst_url,d.DEFAULT_UDP_PORT,self.RESOURCE)
         log.debug('Following DELETE command entered:'+ ",".join( str(c) for c in params))
-        res=self.coap.DELETE(uri=url,payload=params)
-        
-   
+        try:
+           res=self.coap.DELETE(uri=url,payload=params)
+        except Exception as err:
+            log.error("Cannot access the resource {0}:{1}".format(url,err))
+            print "Cannot access the resource {0}:{1}".format(url,err)
+            
     def quit(self):
         raise NotImplementedError()
     
@@ -101,3 +137,34 @@ class sixtusCoap(object):
    
     
     #======================== private =========================================
+    
+    def hex2buf(self,s):
+        '''
+        \brief Convert a string of hex caracters into a byte list.
+        
+        That is: 'abcdef00' -> [0xab,0xcd,0xef,0x00]
+        
+        \param[in] s The string to convert
+        
+        \returns A list of integers, each element in [0x00..0xff].
+        '''
+        assert type(s)==str
+        assert len(s)%2 == 0
+    
+        returnVal = []
+    
+        for i in range(len(s)/2):
+            realIdx = i*2
+            returnVal.append(int(s[realIdx:realIdx+2],16))
+    
+        return returnVal
+    
+    def _convertAddress2bytes(self,target_addr):
+         #format target so i can travel in the payload
+        try:
+            target_addr=target_addr.split("::")[1]
+        except:#if it does not have :: nothing happens
+            pass 
+        newstr = target_addr.replace(":", "")
+        return self.hex2buf(newstr)
+    
