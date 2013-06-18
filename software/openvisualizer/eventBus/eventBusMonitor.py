9cc9ccae579089842fd1cdc83bd96af006628e6d
Thomas Watteyne <twatteyne@gmail.com>
2013-06-17 23:27:44 -0700

    Show 6LoWPAN packets received from the LLN on Wireshark. Cleaned up eventBusMonitor.py module.

--- software/openvisualizer/eventBus/eventBusMonitor.py
+++ software/openvisualizer/eventBus/eventBusMonitor.py
@@ -9,7 +9,6 @@ log.addHandler(NullHandler())
 import threading
 import copy
 import json
-import socket
 import binascii
 
 import openvisualizer_utils as u
@@ -27,16 +26,13 @@ class eventBusMonitor(object):
         # store params
         
         # local variables
-        self.dataLock  = threading.Lock()
-        self.stats      = {}
-        self.socket     = socket.socket(
-            socket.AF_INET6,
-            socket.SOCK_DGRAM
-        )
-        self.meshDebugEnabled = False
+        self.dataLock             = threading.Lock()
+        self.stats                = {}
+        self.meshDebugEnabled     = False
+        self.dagRootEui64         = [0x00]*8
         
         # give this instance a name
-        self.name       = 'eventBusMonitor'
+        self.name                 = 'eventBusMonitor'
         
         # connect to dispatcher
         dispatcher.connect(
@@ -78,49 +74,67 @@ class eventBusMonitor(object):
     #======================== private =========================================
     
     def _eventBusNotification(self,signal,sender,data):
-        'Adds the signal to stats log and performs signal-specific handling'
+        '''
+        Adds the signal to stats log and performs signal-specific handling
+        '''
         
         with self.dataLock:
             key = (sender,signal)
             if key not in self.stats:
                 self.stats[key] = 0
             self.stats[key] += 1
-
+        
+        if signal=='infoDagRoot':
+            self.dagRootEui64 = data['eui64'][:]
+        
+        if signal=='fromMote.data' and self.meshDebugEnabled:
+            (previousHop,lowpan) = data
+            
+            
+            zep = self._wrapZepHeaders(previousHop, self.dagRootEui64, lowpan)
+            self._dispatchMeshDebugPacket(zep)
+            
         if signal=='bytesToMesh' and self.meshDebugEnabled:
             # Forwards a copy of the 6LoWPAN packet destined for the mesh 
             # to the Internet interface for debugging.
             (nextHop,lowpan) = data
             
-            zep = self._wrapZepHeaders(lowpan)
+            zep = self._wrapZepHeaders(self.dagRootEui64,nextHop,lowpan)
             self._dispatchMeshDebugPacket(zep)
             
-    def _wrapZepHeaders(self, lowpan):
+    def _wrapZepHeaders(self, previousHop, nextHop, lowpan):
         '''
         Returns Exegin ZEP protocol header and dummy 802.15.4 header 
         wrapped around outgoing 6LoWPAN layer packet.
         '''
-        # IEEE802.15.4                   (data frame with dummy values)
-        mac    = [0x41,0xcc]             # frame control
-        mac   += [0x66]                  # sequence number
-        mac   += [0xff,0xff]             # destination PAN ID
-        mac   += [0x01]*8                # destination address
-        mac   += [0x02]*8                # source address
+        
+        phop   = previousHop[:]
+        phop.reverse()
+        nhop   = nextHop[:]
+        nhop.reverse()
+        
+        # ZEP
+        zep    = [ord('E'),ord('X')]   # Protocol ID String
+        zep   += [0x02]                # Protocol Version
+        zep   += [0x01]                # Type
+        zep   += [0x00]                # Channel ID
+        zep   += [0x00,0x01]           # Device ID
+        zep   += [0x01]                # LQI/CRC mode
+        zep   += [0xff]
+        zep   += [0x01]*8              # timestamp
+        zep   += [0x02]*4              # sequence number
+        zep   += [0x00]*10             # reserved
+        zep   += [21+len(lowpan)+2]    # length
+        
+        # IEEE802.15.4                 (data frame with dummy values)
+        mac    = [0x41,0xcc]           # frame control
+        mac   += [0x66]                # sequence number
+        mac   += [0xca,0xfe]           # destination PAN ID
+        mac   += nhop                  # destination address
+        mac   += phop                  # source address
         mac   += lowpan
         # CRC
         mac   += u.calculateFCS(mac)
-        
-        # ZEP
-        zep    = [ord('E'),ord('X')]     # Protocol ID String
-        zep   += [0x02]                  # Protocol Version
-        zep   += [0x01]                  # Type
-        zep   += [0x00]                  # Channel ID
-        zep   += [0x00,0x01]             # Device ID
-        zep   += [0x01]                  # LQI/CRC mode
-        zep   += [0xff]
-        zep   += [0x01]*8                # timestamp
-        zep   += [0x02]*4                # sequence number
-        zep   += [0x00]*10               # reserved
-        zep   += [21+len(lowpan)+2]      # length
         
         return zep+mac
         
@@ -130,6 +144,7 @@ class eventBusMonitor(object):
         with UDP and IPv6 headers. Then forwards as an event to the 
         Internet interface.
         '''
+        
         # UDP
         udplen  = len(zep)+8
         
@@ -144,11 +159,10 @@ class eventBusMonitor(object):
         addr   += openTun.IPV6PREFIX
         addr   += openTun.IPV6HOST
         
-        # CRC  See https://tools.ietf.org/html/rfc2460.
+        # CRC See https://tools.ietf.org/html/rfc2460.
       
-        #not sure if the payload contains the udp header in this case.
+        # not sure if the payload contains the udp header in this case.
         udp[6:8] = u.calculatePseudoHeaderCRC(addr,addr,[0x00,0x00]+udp[4:6],[0x00,0x00,0x00,17],zep)
-        
         
         # IPv6
         ip     = [6<<4]                  # v6 + traffic class (upper nybble)
@@ -161,6 +175,7 @@ class eventBusMonitor(object):
         ip    += udp
         
         dispatcher.send(
-                    sender=self.name,
-                    signal='v6ToInternet',
-                    data  =ip)
+            sender = self.name,
+            signal = 'v6ToInternet',
+            data   = ip,
+        )
