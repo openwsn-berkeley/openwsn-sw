fe7e1536e218fe1475bff76db65bc16e85cfd183
Xavier Vilajosana <xvilajosana@eecs.berkeley.edu>
2013-06-21 14:21:14 -0700

    SW-61. Adding tracing logger using yappi. -t enables the tracer when running OV

---
+++ software/openvisualizer/OVtracer.py
@@ -0,0 +1,33 @@
+import logging
+
+class NullHandler(logging.Handler):
+    def emit(self, record):
+        pass
+log = logging.getLogger('OVtracer')
+log.setLevel(logging.DEBUG)
+log.addHandler(NullHandler())
+
+
+import threading
+import yappi
+
+class OVtracer(object):
+    TRACING_INTERVAL = 4
+    
+    def __init__(self):
+            yappi.start()
+            self.timer = threading.Timer(self.TRACING_INTERVAL,self._logTracingStats)
+            self.timer.start()
+            
+    def _logTracingStats(self):
+         yappi.enum_thread_stats(self._logThreadStat)
+         #yappi.enum_stats(self._logFunctionStat)
+         
+    def _logThreadStat(self,stat_entry):
+         log.info("Thread Trace: {0}".format(stat_entry))
+         #print "Thread Trace: {0}".format(stat_entry)
+    
+    def _logFunctionStat(self,stat_entry):
+         log.info("Function Trace: {0}".format(stat_entry))
+         #print "Function Trace: {0}".format(stat_entry)
+              
--- software/openvisualizer/bin/openVisualizerGui/openVisualizerGui.py
+++ software/openvisualizer/bin/openVisualizerGui/openVisualizerGui.py
@@ -185,11 +185,12 @@ class OpenVisualizerGui(object):
     
 class OpenVisualizerGui_app(object):
     
-    def __init__(self,simulatorMode,numMotes):
+    def __init__(self,simulatorMode,numMotes,trace):
         
         # store params
         self.simulatorMode        = simulatorMode
         self.numMotes             = numMotes
+        self.trace                = trace 
         
         # local variables
         self.eventBusMonitor      = eventBusMonitor.eventBusMonitor()
@@ -252,9 +253,16 @@ class OpenVisualizerGui_app(object):
                 )
             self.simengine.resume()
         
+        # start tracing threads
+        if self.trace:
+            import OVtracer
+            appDir = '.'
+            logging.config.fileConfig(os.path.join(appDir,'trace.conf'), {'logDir': appDir})
+            OVtracer.OVtracer()
         # start the GUI
         gui.start()
     
+       
     #======================== GUI callbacks ===================================
 
 #============================ main ============================================
@@ -274,14 +282,20 @@ def parseCliOptions():
         default    = 3,
     )
     
+    parser.add_option( '--trace','-t',
+        dest       = 'trace',
+        default    = False,
+        action     = 'store_true',
+    )
+    
     (opts, args)  = parser.parse_args()
     
     return opts
 
-def main(simulatorMode,numMotes):
+def main(simulatorMode,numMotes,trace):
     appDir = '.'
     logging.config.fileConfig(os.path.join(appDir,'logging.conf'), {'logDir': appDir})
-    app = OpenVisualizerGui_app(simulatorMode,numMotes)
+    app = OpenVisualizerGui_app(simulatorMode,numMotes,trace)
 
 if __name__=="__main__":
     
@@ -291,4 +305,5 @@ if __name__=="__main__":
     main(
         simulatorMode   = args['simulatorMode'],
         numMotes        = args['numMotes'],
+        trace           = args['trace'],
     )
