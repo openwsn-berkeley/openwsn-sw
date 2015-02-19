8f48bbe75dc7134fdc116e426c3327dd35eccf69
Vincent <vincent.noyalet@telecomnancy.eu>
2015-02-19 02:22:35 +0100

    After 1 day of work, the code takes 3 lines ... don't know if it's a sucess or a failure

--- software/openvisualizer/bin/openVisualizerApp/openVisualizerWeb.py
+++ software/openvisualizer/bin/openVisualizerApp/openVisualizerWeb.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # Copyright (c) 2013, Ken Bannister.
 # All rights reserved.
-# 
+#
 # Released under the BSD 2-Clause license as published at the link below.
 # http://opensource.org/licenses/BSD-2-Clause
 import sys
@@ -32,7 +32,8 @@ import re
 import threading
 import signal
 import functools
-from bottle        import view
+import datetime
+from bottle        import view, response
 
 import openVisualizerApp
 import openvisualizer.openvisualizer_utils as u
@@ -52,35 +53,35 @@ class OpenVisualizerWeb(eventBusClient.eventBusClient):
     Provides web UI for OpenVisualizer. Runs as a webapp in a Bottle web
     server.
     '''
-    
+
     def __init__(self,app,websrv):
         '''
         :param app:    OpenVisualizerApp
         :param websrv: Web server
         '''
         log.info('Creating OpenVisualizerWeb')
-        
+
         # store params
         self.app             = app
         self.engine          = SimEngine.SimEngine()
         self.websrv          = websrv
-        
+
         self._defineRoutes()
-        
+
         # To find page templates
         bottle.TEMPLATE_PATH.append('{0}/web_files/templates/'.format(self.app.datadir))
-        
+
         # initialize parent class
         eventBusClient.eventBusClient.__init__(
             self,
             name                  = 'OpenVisualizerWeb',
             registrations         =  [],
         )
-    
+
     #======================== public ==========================================
-    
+
     #======================== private =========================================
-    
+
     def _defineRoutes(self):
         '''
         Matches web URL to impelementing method. Cannot use @route annotations
@@ -99,6 +100,7 @@ class OpenVisualizerWeb(eventBusClient.eventBusClient):
         self.websrv.route(path='/gologicDebug/:enabled',                  callback=self._setGologicDebug)
         self.websrv.route(path='/topology',                               callback=self._topologyPage)
         self.websrv.route(path='/topology/data',                          callback=self._topologyData)
+        self.websrv.route(path='/topology/download',                      callback=self._topologyDownload)
         self.websrv.route(path='/topology/motes',         method='POST',  callback=self._topologyMotesUpdate)
         self.websrv.route(path='/topology/connections',   method='PUT',   callback=self._topologyConnectionsCreate)
         self.websrv.route(path='/topology/connections',   method='POST',  callback=self._topologyConnectionsUpdate)
@@ -106,16 +108,16 @@ class OpenVisualizerWeb(eventBusClient.eventBusClient):
         self.websrv.route(path='/topology/route',         method='GET',   callback=self._topologyRouteRetrieve)
         self.websrv.route(path='/topology/create',                        callback=self._topologyCreate)
         self.websrv.route(path='/static/<filepath:path>',                 callback=self._serverStatic)
-    
+
     @view('moteview.tmpl')
     def _showMoteview(self, moteid=None):
         '''
         Collects the list of motes, and the requested mote to view.
-        
+
         :param moteid: 16-bit ID of mote (optional)
         '''
         log.debug("moteview moteid parameter is {0}".format(moteid));
-        
+
         motelist = []
         for ms in self.app.moteStates:
             addr = ms.getStateElem(moteState.moteState.ST_IDMANAGER).get16bAddr()
@@ -123,22 +125,22 @@ class OpenVisualizerWeb(eventBusClient.eventBusClient):
                 motelist.append( ''.join(['%02x'%b for b in addr]) )
             else:
                 motelist.append(ms.moteConnector.serialport)
-        
+
         tmplData = {
             'motelist'       : motelist,
             'requested_mote' : moteid if moteid else 'none',
         }
         return tmplData
-    
+
     def _serverStatic(self, filepath):
-        return bottle.static_file(filepath, 
+        return bottle.static_file(filepath,
                                   root='{0}/web_files/static/'.format(self.app.datadir))
-    
+
     def _toggleDAGroot(self, moteid):
         '''
         Triggers toggle DAGroot state, via moteState. No real response. Page is
         updated when next retrieve mote data.
-        
+
         :param moteid: 16-bit ID of mote
         '''
         log.info('Toggle root status for moteid {0}'.format(moteid))
@@ -150,11 +152,11 @@ class OpenVisualizerWeb(eventBusClient.eventBusClient):
         else:
             log.debug('Mote {0} not found in moteStates'.format(moteid))
             return '{"result" : "fail"}'
-    
+
     def _getMoteData(self, moteid):
         '''
         Collects data for the provided mote.
-        
+
         :param moteid: 16-bit ID of mote
         '''
         log.debug('Get JSON data for moteid {0}'.format(moteid))
@@ -178,51 +180,51 @@ class OpenVisualizerWeb(eventBusClient.eventBusClient):
             log.debug('Mote {0} not found in moteStates'.format(moteid))
             states = {}
         return states
-    
+
     def _setWiresharkDebug(self, enabled):
         '''
         Selects whether eventBus must export debug packets.
-        
+
         :param enabled: 'true' if enabled; any other value considered false
         '''
         log.info('Enable wireshark debug : {0}'.format(enabled))
         self.app.eventBusMonitor.setWiresharkDebug(enabled == 'true')
         return '{"result" : "success"}'
-    
+
     def _setGologicDebug(self, enabled):
         log.info('Enable GoLogic debug : {0}'.format(enabled))
         VcdLogger.VcdLogger().setEnabled(enabled == 'true')
         return '{"result" : "success"}'
-    
+
     @view('eventBus.tmpl')
     def _showEventBus(self):
         '''
-        Simple page; data for the page template is identical to the data 
+        Simple page; data for the page template is identical to the data
         for periodic updates of event list.
         '''
         return self._getEventData()
-    
+
     def _showDAG(self):
-        states,edges = self.app.topology.getDAG()  
+        states,edges = self.app.topology.getDAG()
         return { 'states': states, 'edges': edges }
-    
+
     @view('routing.tmpl')
     def _showRouting(self):
         return {}
-        
+
     @view('topology.tmpl')
     def _topologyPage(self):
         '''
         Retrieve the HTML/JS page.
         '''
-        
+
         return {}
-    
+
     def _topologyData(self):
         '''
         Retrieve the topology data, in JSON format.
         '''
-        
+
         # motes
         motes = []
         rank  = 0
@@ -241,19 +243,19 @@ class OpenVisualizerWeb(eventBusClient.eventBusClient):
                 rank+=1
             except IndexError:
                break
-        
+
         # connections
         connections = self.engine.propagation.retrieveConnections()
-        
+
         data = {
             'motes'          : motes,
             'connections'    : connections,
         }
-        
+
         return data
-    
+
     def _topologyMotesUpdate(self):
-        
+
         motesTemp = {}
         for (k,v) in bottle.request.forms.items():
             m = re.match("motes\[(\w+)\]\[(\w+)\]", k)
@@ -270,67 +272,111 @@ class OpenVisualizerWeb(eventBusClient.eventBusClient):
             if index not in motesTemp:
                 motesTemp[index] = {}
             motesTemp[index][param] = v
-        
+
         for (_,v) in motesTemp.items():
             mh = self.engine.getMoteHandlerById(v['id'])
             mh.setLocation(v['lat'],v['lon'])
-    
+
     def _topologyConnectionsCreate(self):
-        
+
         data = bottle.request.forms
         assert sorted(data.keys())==sorted(['fromMote', 'toMote'])
-        
+
         fromMote = int(data['fromMote'])
         toMote   = int(data['toMote'])
-        
+
         self.engine.propagation.createConnection(fromMote,toMote)
-    
+
     def _topologyConnectionsUpdate(self):
         data = bottle.request.forms
         assert sorted(data.keys())==sorted(['fromMote', 'toMote', 'pdr'])
-        
+
         fromMote = int(data['fromMote'])
         toMote   = int(data['toMote'])
         pdr      = float(data['pdr'])
-        
+
         self.engine.propagation.updateConnection(fromMote,toMote,pdr)
-    
+
     def _topologyConnectionsDelete(self):
-        
+
         data = bottle.request.forms
         assert sorted(data.keys())==sorted(['fromMote', 'toMote'])
-        
+
         fromMote = int(data['fromMote'])
         toMote   = int(data['toMote'])
-        
+
         self.engine.propagation.deleteConnection(fromMote,toMote)
-    
+
     def _topologyRouteRetrieve(self):
-        
+
         data = bottle.request.query
-        
+
         assert data.keys()==['destination']
-        
+
         detination_eui = [0x14,0x15,0x92,0xcc,0x00,0x00,0x00,int(data['destination'])]
-        
+
         route = self._dispatchAndGetResult(
-            signal       = 'getSourceRoute', 
+            signal       = 'getSourceRoute',
             data         = detination_eui,
         )
-        
+
         route = [r[-1] for r in route]
-        
+
         data = {
             'route'          : route,
         }
-        
+
         return data
+
+    #@view('download.tmpl')
+    def _topologyDownload(self):
+        '''
+        Retrieve the topology data, in JSON format, and download it.
+        '''
+
+        # motes
+        motes = []
+        rank  = 0
+        while True:
+            try:
+                mh            = self.engine.getMoteHandler(rank)
+                id            = mh.getId()
+                (lat,lon)     = mh.getLocation()
+                motes += [
+                    {
+                        'id':    id,
+                        'lat':   lat,
+                        'lon':   lon,
+                    }
+                ]
+                rank+=1
+            except IndexError:
+               break
+
+        # connections
+        connections = self.engine.propagation.retrieveConnections()
+
+        data = {
+            'motes'          : motes,
+            'connections'    : connections,
+        }
+        
+        #file
+        now = datetime.datetime.now()
+
+        response.headers['Content-disposition']='attachement; filename=topology_data_'+now.strftime("%d-%m-%y_%Hh%M")+'.json'
+        response.headers['filename']='test.json'
+        response.headers['Content-type']= 'application/json'
+
+        return data
+
+
 
     @view('create.tmpl')
     def _topologyCreate(self):
 
         print "coucou"
-  
+
     def _getEventData(self):
         response = {
             'isDebugPkts' : 'true' if self.app.eventBusMonitor.wiresharkDebugEnabled else 'false',
@@ -343,14 +389,14 @@ from argparse       import ArgumentParser
 
 def _addParserArgs(parser):
     '''Adds arguments specific to web UI.'''
-    
+
     parser.add_argument('-H', '--host',
         dest       = 'host',
         default    = '0.0.0.0',
         action     = 'store',
         help       = 'host address'
     )
-    
+
     parser.add_argument('-p', '--port',
         dest       = 'port',
         default    = 8080,
@@ -363,7 +409,7 @@ if __name__=="__main__":
     parser   =  ArgumentParser()
     _addParserArgs(parser)
     argspace = parser.parse_known_args()[0]
-    
+
     # log
     log.info(
         'Initializing OpenVisualizerWeb with options: \n\t{0}'.format(
@@ -375,14 +421,14 @@ if __name__=="__main__":
             )
         )
     )
-    
+
     #===== start the app
     app      = openVisualizerApp.main(parser)
-    
+
     #===== add a web interface
     websrv   = bottle.Bottle()
     webapp   = OpenVisualizerWeb(app, websrv)
-    
+
     # start web interface in a separate thread
     webthread = threading.Thread(
         target = websrv.run,
@@ -394,16 +440,16 @@ if __name__=="__main__":
         }
     )
     webthread.start()
-    
+
     #===== add a cli (minimal) interface
-    
+
     banner  = []
     banner += ['OpenVisualizer']
     banner += ['web interface started at {0}:{1}'.format(argspace.host,argspace.port)]
     banner += ['enter \'q\' to exit']
     banner  = '\n'.join(banner)
     print banner
-    
+
     while True:
         input = raw_input('> ')
         if input=='q':
