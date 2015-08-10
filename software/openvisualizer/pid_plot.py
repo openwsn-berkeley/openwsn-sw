8c8ba6d45b31b79c2b6b706eca3076c3fb42f90e
changtengfei <tengfei.chang@gmail.com>
2015-08-10 12:14:46 +0800

    FW-269. fixed depending on the printed out message on firmware.

--- software/openvisualizer/pid_plot.py
+++ software/openvisualizer/pid_plot.py
@@ -10,8 +10,9 @@ import os
 # =============== structure ===============
 slotframe = []
 numOfCell = []
-pidError  = []
-        
+packetInQueue = []
+sentPacket = []
+traffic = []        
 # =============== prototype ===============
 
 output = open('output.txt','r')
@@ -21,35 +22,49 @@ while line != '':
         data = line.split(',')
         slotframe.append(int(data[0]))
         numOfCell.append(int(data[1]))
-        pidError.append(int(data[2]))
+        packetInQueue.append(int(data[2]))
+        sentPacket.append(int(data[2]))
+        traffic.append(int(data[2]))
     line = output.readline()
 output.close()
         
 x_slotframe = np.int_(slotframe)
 y_numOfCell = np.int_(numOfCell)
-y_pidError  = np.int_(pidError) 
+y_packetInQueue = np.int_(packetInQueue) 
+y_sentPacket = np.int_(sentPacket)
+y_traffic = np.int_(traffic)
 
-z1 = np.polyfit(x_slotframe,y_numOfCell,10)
-z2 = np.polyfit(x_slotframe,y_pidError,10)
+z1 = np.polyfit(x_slotframe,y_numOfCell,12)
+z2 = np.polyfit(x_slotframe,y_packetInQueue,12)
+z3 = np.polyfit(x_slotframe,y_sentPacket,12)
+z4 = np.polyfit(x_slotframe,y_traffic,12)
 
 fit1 = np.poly1d(z1)
 fit2 = np.poly1d(z2)
+fit3 = np.poly1d(z3)
+fit4 = np.poly1d(z4)
 
 y_new1 = fit1(x_slotframe)
 y_new2 = fit2(x_slotframe)
+y_new3 = fit3(x_slotframe)
+y_new4 = fit4(x_slotframe)
 
 #================ raw data ====================
 plt.plot(x_slotframe,y_numOfCell,label='numOfCell')
-plt.plot(x_slotframe,y_pidError,label='pidError')
+plt.plot(x_slotframe,y_packetInQueue,label='packetInQueue')
+plt.plot(x_slotframe,y_sentPacket,label='sentPacket')
+plt.plot(x_slotframe,y_traffic,label='traffic')
 
 #================  curve fitting ==============
 # plt.plot(x_slotframe,y_new1,label='numOfCell')
-# plt.plot(x_slotframe,y_new2,label='pidError')
+# plt.plot(x_slotframe,y_new2,label='packetInQueue')
+# plt.plot(x_slotframe,y_new3,label='sentPacket')
+# plt.plot(x_slotframe,y_new4,label='traffic')
 
 legend = plt.legend(shadow=True, fontsize='x-large')
 
 plt.xlim(slotframe[0], slotframe[-1])
 plt.xlabel('slotframe')
-plt.title('pid test')
+plt.title('pid test (kp:1,ki:0.002,kd:-0.01)')
 
 plt.show()
