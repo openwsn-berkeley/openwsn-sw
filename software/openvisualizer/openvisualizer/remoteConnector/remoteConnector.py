dc8fa8f1b53e87ca08cb61fa048462cc8db97cf5
Heriam <haojiang@kth.se>
2016-03-30 12:08:29 +0200

    Adapting ZeroMQ

--- software/openvisualizer/openvisualizer/remoteConnector/remoteConnector.py
+++ software/openvisualizer/openvisualizer/remoteConnector/remoteConnector.py
@@ -13,6 +13,7 @@ import socket
 import traceback
 import sys
 import openvisualizer.openvisualizer_utils as u
+import zmq
 
 from pydispatch import dispatcher
 
@@ -24,20 +25,24 @@ import ParserException
 
 class moteConnector(eventBusClient.eventBusClient):
     
-    def __init__(self,roverMote):
+    def __init__(self, iplist=[]):
         
         # log
         log.info("creating instance")
-        
-        # store params
-        self.roverMote                = roverMote
-        
+
         # local variables
         self.parser                    = OpenParser.OpenParser()
         self.stateLock                 = threading.Lock()
         self.networkPrefix             = None
         self._subcribedDataForDagRoot  = False
-              
+        self.iplist = iplist
+        self.context = zmq.Context()
+
+        self.publisher = self.context.socket(zmq.PUB)
+        self.publisher.bind("tcp://*:50000")
+
+
+
         # give this thread a name
         self.name = 'remoteConnector@{0}'.format(self.roverMote)
        
@@ -48,13 +53,18 @@ class moteConnector(eventBusClient.eventBusClient):
                 {
                     'sender'   : self.WILDCARD,
                     'signal'   : 'infoDagRoot',
-                    'callback' : self._infoDagRoot_handler,
+                    'callback' : self._sendToRemote_handler,
                 },
                 {
                     'sender'   : self.WILDCARD,
                     'signal'   : 'cmdToMote',
-                    'callback' : self._cmdToMote_handler,
+                    'callback' : self._sendToRemote_handler,
                 },
+                {
+                    'sender'   : self.WILDCARD,
+                    'signal'   : 'bytesToMesh',
+                    'callback' : self._sendToRemote_handler,
+                }
             ]
         )
         
@@ -86,39 +96,41 @@ class moteConnector(eventBusClient.eventBusClient):
         
     #======================== eventBus interaction ============================
     
-    def _infoDagRoot_handler(self,sender,signal,data):
+    def _sendToRemote_handler(self,sender,signal,data):
+
+        self.publisher.send_json({'sender' : sender, 'signal' : signal, 'data':data})
         
-        # I only care about "infoDagRoot" notifications about my mote
-        if not data['serialPort']==self.roverMote:
-            return 
-        
-        with self.stateLock:
-        
-            if   data['isDAGroot']==1 and (not self._subcribedDataForDagRoot):
-                # this moteConnector is connected to a DAGroot
-                
-                # connect to dispatcher
-                self.register(
-                    sender   = self.WILDCARD,
-                    signal   = 'bytesToMesh',
-                    callback = self._bytesToMesh_handler,
-                )
-                
-                # remember I'm subscribed
-                self._subcribedDataForDagRoot = True
-                
-            elif data['isDAGroot']==0 and self._subcribedDataForDagRoot:
-                # this moteConnector is *not* connected to a DAGroot
-                
-                # disconnect from dispatcher
-                self.unregister(
-                    sender   = self.WILDCARD,
-                    signal   = 'bytesToMesh',
-                    callback = self._bytesToMesh_handler,
-                )
-                
-                # remember I'm not subscribed
-                self._subcribedDataForDagRoot = False
+        # # I only care about "infoDagRoot" notifications about my mote
+        # if not data['serialPort']==self.roverMote:
+        #     return
+        #
+        # with self.stateLock:
+        #
+        #     if   data['isDAGroot']==1 and (not self._subcribedDataForDagRoot):
+        #         # this moteConnector is connected to a DAGroot
+        #
+        #         # connect to dispatcher
+        #         self.register(
+        #             sender   = self.WILDCARD,
+        #             signal   = 'bytesToMesh',
+        #             callback = self._bytesToMesh_handler,
+        #         )
+        #
+        #         # remember I'm subscribed
+        #         self._subcribedDataForDagRoot = True
+        #
+        #     elif data['isDAGroot']==0 and self._subcribedDataForDagRoot:
+        #         # this moteConnector is *not* connected to a DAGroot
+        #
+        #         # disconnect from dispatcher
+        #         self.unregister(
+        #             sender   = self.WILDCARD,
+        #             signal   = 'bytesToMesh',
+        #             callback = self._bytesToMesh_handler,
+        #         )
+        #
+        #         # remember I'm not subscribed
+        #         self._subcribedDataForDagRoot = False
     
     def _cmdToMote_handler(self,sender,signal,data):
         if  data['serialPort']==self.serialport:
@@ -160,93 +172,96 @@ class moteConnector(eventBusClient.eventBusClient):
             else:
                 raise SystemError('unexpected action={0}'.format(data['action']))
     
-    def _GDcommandToBytes(self,data):
-        
-        outcome    = False
-        dataToSend = []
+    # def _GDcommandToBytes(self,data):
+    #
+    #     outcome    = False
+    #     dataToSend = []
+    #
+    #     # get imageId
+    #     if data[0] == 'gd_root':
+    #         imageId  = 1
+    #     elif data[0] == 'gd_sniffer':
+    #         imageId = 2
+    #     else:
+    #         print "============================================="
+    #         print "Wrong Image ({0})! (Available: gd_root OR gd_sniffer)\n".format(data[0])
+    #         return [outcome,dataToSend]
+    #
+    #     # get commandId
+    #     commandIndex = 0
+    #     for cmd in moteState.moteState.COMMAND_ALL:
+    #         if data[1] == cmd[0]:
+    #             commandId  = cmd[1]
+    #             commandLen = cmd[2]
+    #             break
+    #         else:
+    #             commandIndex += 1
+    #
+    #     # check avaliability of command
+    #     if commandIndex == len(moteState.moteState.COMMAND_ALL):
+    #         print "============================================="
+    #         print "Wrong Command Type! Available Command Type: {"
+    #         for cmd in moteState.moteState.COMMAND_ALL:
+    #             print " {0}".format(cmd[0])
+    #         print " }"
+    #         return [outcome,dataToSend]
+    #
+    #     if data[1][:2] == '6p':
+    #         try:
+    #             dataToSend = [OpenParser.OpenParser.SERFRAME_PC2MOTE_COMMAND_GD,
+    #                 2, # version
+    #                 imageId,
+    #                 commandId,
+    #                 len(data[2][1:-1].split(','))
+    #             ]
+    #             if data[1] == '6pAdd' or data[1] == '6pDelete':
+    #                 if len(data[2][1:-1].split(','))>0:
+    #                     dataToSend += [int(i) for i in data[2][1:-1].split(',')] # celllist
+    #         except:
+    #             print "============================================="
+    #             print "Wrong 6p parameter format {0}. Split the slot by".format(data[2])
+    #             print "comma. e.g. 6,7. (up to 3)"
+    #             return [outcome,dataToSend]
+    #     else:
+    #         parameter = int(data[2])
+    #         if parameter <= 0xffff:
+    #             parameter  = [(parameter & 0xff),((parameter >> 8) & 0xff)]
+    #             dataToSend = [OpenParser.OpenParser.SERFRAME_PC2MOTE_COMMAND_GD,
+    #                 2, # version
+    #                 imageId,
+    #                 commandId,
+    #                 commandLen, # length
+    #                 parameter[0],
+    #                 parameter[1]
+    #             ]
+    #         else:
+    #             # more than two bytes parameter, error
+    #             print "============================================="
+    #             print "Paramter Wrong! (Available: 0x0000~0xffff)\n"
+    #             return [outcome,dataToSend]
+    #
+    #     # the command is legal if I got here
+    #     outcome = True
+    #     return [outcome,dataToSend]
 
-        # get imageId
-        if data[0] == 'gd_root':
-            imageId  = 1
-        elif data[0] == 'gd_sniffer':
-            imageId = 2
-        else:
-            print "============================================="
-            print "Wrong Image ({0})! (Available: gd_root OR gd_sniffer)\n".format(data[0])
-            return [outcome,dataToSend]
 
-        # get commandId
-        commandIndex = 0
-        for cmd in moteState.moteState.COMMAND_ALL:
-            if data[1] == cmd[0]:
-                commandId  = cmd[1]
-                commandLen = cmd[2]
-                break
-            else:
-                commandIndex += 1
-
-        # check avaliability of command
-        if commandIndex == len(moteState.moteState.COMMAND_ALL):
-            print "============================================="
-            print "Wrong Command Type! Available Command Type: {"
-            for cmd in moteState.moteState.COMMAND_ALL:
-                print " {0}".format(cmd[0])
-            print " }"
-            return [outcome,dataToSend]
-
-        if data[1][:2] == '6p':
-            try:
-                dataToSend = [OpenParser.OpenParser.SERFRAME_PC2MOTE_COMMAND_GD,
-                    2, # version
-                    imageId,
-                    commandId,
-                    len(data[2][1:-1].split(','))
-                ]
-                if data[1] == '6pAdd' or data[1] == '6pDelete':
-                    if len(data[2][1:-1].split(','))>0:
-                        dataToSend += [int(i) for i in data[2][1:-1].split(',')] # celllist
-            except:
-                print "============================================="
-                print "Wrong 6p parameter format {0}. Split the slot by".format(data[2])
-                print "comma. e.g. 6,7. (up to 3)"
-                return [outcome,dataToSend]
-        else:
-            parameter = int(data[2])
-            if parameter <= 0xffff:
-                parameter  = [(parameter & 0xff),((parameter >> 8) & 0xff)]
-                dataToSend = [OpenParser.OpenParser.SERFRAME_PC2MOTE_COMMAND_GD,
-                    2, # version
-                    imageId,
-                    commandId,
-                    commandLen, # length 
-                    parameter[0],
-                    parameter[1]
-                ]
-            else:
-                # more than two bytes parameter, error
-                print "============================================="
-                print "Paramter Wrong! (Available: 0x0000~0xffff)\n"
-                return [outcome,dataToSend]
-
-        # the command is legal if I got here
-        outcome = True
-        return [outcome,dataToSend]
-
-
-    def _bytesToMesh_handler(self,sender,signal,data):
-        assert type(data)==tuple
-        assert len(data)==2
-        
-        (nextHop,lowpan) = data
-        
-        self._sendToMoteProbe(
-            dataToSend = [OpenParser.OpenParser.SERFRAME_PC2MOTE_DATA]+nextHop+lowpan,
-        )
+    # def _bytesToMesh_handler(self,sender,signal,data):
+    #     assert type(data)==tuple
+    #     assert len(data)==2
+    #
+    #     (nextHop,lowpan) = data
+    #
+    #     self._sendToMoteProbe(
+    #         dataToSend = [OpenParser.OpenParser.SERFRAME_PC2MOTE_DATA]+nextHop+lowpan,
+    #     )
     
     #======================== public ==========================================
     
     def quit(self):
         raise NotImplementedError()
+
+    def addRaspi(self, ip):
+        self.iplist.append(ip)
     
     #======================== private =========================================
     
