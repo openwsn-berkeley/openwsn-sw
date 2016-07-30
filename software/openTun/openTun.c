6e3b3b1dc5b7287e01953f2626e620ef7c923126
Michael Richardson <mcr@sandelman.ca>
2016-07-30 17:27:56 +0000

    decode hdlc, validate packet type byte, and decode status

--- software/openTun/openTun.c
+++ software/openTun/openTun.c
@@ -47,19 +47,277 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
+//#include "opendefs.h"
 const char *progname;
+
+enum serframe_constants {
+  SERFRAME_MOTE2PC_DATA   = 'D',
+  SERFRAME_MOTE2PC_STATUS = 'S',
+  SERFRAME_MOTE2PC_INFO   = 'I',
+  SERFRAME_MOTE2PC_ERROR  = 'E',
+  SERFRAME_MOTE2PC_CRITICAL='C',
+  SERFRAME_MOTE2PC_REQUEST ='R',
+  SERFRAME_MOTE2PC_SNIFFED_PACKET ='P',
+
+  SERFRAME_PC2MOTE_SETDAGROOT= 'R',
+  SERFRAME_PC2MOTE_DATA    ='D',
+  SERFRAME_PC2MOTE_TRIGGERSERIALECHO = 'S',
+  SERFRAME_PC2MOTE_COMMAND_GD        = 'G',
+
+  SERFRAME_ACTION_YES      ='Y',
+  SERFRAME_ACTION_NO       ='N',
+  SERFRAME_ACTION_TOGGLE   ='T',
+};
+
+enum inputstate {
+  START = 0,
+  PROCESSING,
+  ESCAPED,
+  ENDFRAME
+};
+
+enum hdlc {
+  HDLC_FLAG              = 0x7e,
+  HDLC_FLAG_ESCAPED      = 0x5e,
+  HDLC_ESCAPE            = 0x7d,
+  HDLC_ESCAPE_ESCAPED    = 0x5d,
+  HDLC_CRCINIT           = 0xffff,
+  HDLC_CRCGOOD           = 0xf0b8,
+};
+
+unsigned short FCS16TAB[]={
+  0x0000, 0x1189, 0x2312, 0x329b, 0x4624, 0x57ad, 0x6536, 0x74bf,
+  0x8c48, 0x9dc1, 0xaf5a, 0xbed3, 0xca6c, 0xdbe5, 0xe97e, 0xf8f7,
+  0x1081, 0x0108, 0x3393, 0x221a, 0x56a5, 0x472c, 0x75b7, 0x643e,
+  0x9cc9, 0x8d40, 0xbfdb, 0xae52, 0xdaed, 0xcb64, 0xf9ff, 0xe876,
+  0x2102, 0x308b, 0x0210, 0x1399, 0x6726, 0x76af, 0x4434, 0x55bd,
+  0xad4a, 0xbcc3, 0x8e58, 0x9fd1, 0xeb6e, 0xfae7, 0xc87c, 0xd9f5,
+  0x3183, 0x200a, 0x1291, 0x0318, 0x77a7, 0x662e, 0x54b5, 0x453c,
+  0xbdcb, 0xac42, 0x9ed9, 0x8f50, 0xfbef, 0xea66, 0xd8fd, 0xc974,
+  0x4204, 0x538d, 0x6116, 0x709f, 0x0420, 0x15a9, 0x2732, 0x36bb,
+  0xce4c, 0xdfc5, 0xed5e, 0xfcd7, 0x8868, 0x99e1, 0xab7a, 0xbaf3,
+  0x5285, 0x430c, 0x7197, 0x601e, 0x14a1, 0x0528, 0x37b3, 0x263a,
+  0xdecd, 0xcf44, 0xfddf, 0xec56, 0x98e9, 0x8960, 0xbbfb, 0xaa72,
+  0x6306, 0x728f, 0x4014, 0x519d, 0x2522, 0x34ab, 0x0630, 0x17b9,
+  0xef4e, 0xfec7, 0xcc5c, 0xddd5, 0xa96a, 0xb8e3, 0x8a78, 0x9bf1,
+  0x7387, 0x620e, 0x5095, 0x411c, 0x35a3, 0x242a, 0x16b1, 0x0738,
+  0xffcf, 0xee46, 0xdcdd, 0xcd54, 0xb9eb, 0xa862, 0x9af9, 0x8b70,
+  0x8408, 0x9581, 0xa71a, 0xb693, 0xc22c, 0xd3a5, 0xe13e, 0xf0b7,
+  0x0840, 0x19c9, 0x2b52, 0x3adb, 0x4e64, 0x5fed, 0x6d76, 0x7cff,
+  0x9489, 0x8500, 0xb79b, 0xa612, 0xd2ad, 0xc324, 0xf1bf, 0xe036,
+  0x18c1, 0x0948, 0x3bd3, 0x2a5a, 0x5ee5, 0x4f6c, 0x7df7, 0x6c7e,
+  0xa50a, 0xb483, 0x8618, 0x9791, 0xe32e, 0xf2a7, 0xc03c, 0xd1b5,
+  0x2942, 0x38cb, 0x0a50, 0x1bd9, 0x6f66, 0x7eef, 0x4c74, 0x5dfd,
+  0xb58b, 0xa402, 0x9699, 0x8710, 0xf3af, 0xe226, 0xd0bd, 0xc134,
+  0x39c3, 0x284a, 0x1ad1, 0x0b58, 0x7fe7, 0x6e6e, 0x5cf5, 0x4d7c,
+  0xc60c, 0xd785, 0xe51e, 0xf497, 0x8028, 0x91a1, 0xa33a, 0xb2b3,
+  0x4a44, 0x5bcd, 0x6956, 0x78df, 0x0c60, 0x1de9, 0x2f72, 0x3efb,
+  0xd68d, 0xc704, 0xf59f, 0xe416, 0x90a9, 0x8120, 0xb3bb, 0xa232,
+  0x5ac5, 0x4b4c, 0x79d7, 0x685e, 0x1ce1, 0x0d68, 0x3ff3, 0x2e7a,
+  0xe70e, 0xf687, 0xc41c, 0xd595, 0xa12a, 0xb0a3, 0x8238, 0x93b1,
+  0x6b46, 0x7acf, 0x4854, 0x59dd, 0x2d62, 0x3ceb, 0x0e70, 0x1ff9,
+  0xf78f, 0xe606, 0xd49d, 0xc514, 0xb1ab, 0xa022, 0x92b9, 0x8330,
+  0x7bc7, 0x6a4e, 0x58d5, 0x495c, 0x3de3, 0x2c6a, 0x1ef1, 0x0f78,
+};
+
+void parse_status(unsigned char inBuf[], unsigned int inLen)
+{
+  if(inLen < 4) return;
+  unsigned int moteId = inBuf[0] + inBuf[1]<<8;
+  unsigned int statusElem = inBuf[1];
+
+  printf("moteId %u status: %u --:", moteId, statusElem);
+  switch(statusElem) {
+  case 0:
+    printf("sync\n");
+    break;
+
+  case 1:
+    printf("IdManager\n");
+    break;
+
+  case 2:
+    printf("MyDagRank\n");
+    break;
+
+  case 3:
+    printf("OutputBuffer\n");
+    break;
+
+  case 4:
+    printf("Asn\n");
+    break;
+
+  case 5:
+    printf("MacStats\n");
+    break;
+
+  case 6:
+    printf("ScheduleRow\n");
+    break;
+  }
+
+}
+
+void process_input(unsigned char inBuf[], unsigned int inLen)
+{
+  int i;
+  switch(inBuf[0]) {
+  case SERFRAME_MOTE2PC_DATA:
+    printf("mote2pc data(%u)\n  ", inLen);
+
+    for(i=1; i<inLen; i++) {
+      printf("%02x ", inBuf[i]);
+    }
+    printf("\n");
+    break;
+
+  case SERFRAME_MOTE2PC_STATUS:
+    printf("mote2pc status(%u) \n  ", inLen);
+    parse_status(inBuf, inLen);
+    break;
+
+  case SERFRAME_MOTE2PC_INFO:
+    printf("mote2pc info\n");
+    break;
+  case SERFRAME_MOTE2PC_ERROR:
+    printf("mote2pc error\n");
+    break;
+  case SERFRAME_MOTE2PC_CRITICAL:
+    printf("mote2pc critical\n");
+    break;
+  case SERFRAME_MOTE2PC_REQUEST:
+    printf("mote2pc request(%u)\n  ", inLen);
+    for(i=1; i<inLen; i++) {
+      printf("%02x ", inBuf[i]);
+    }
+    printf("\n");
+    break;
+
+  case SERFRAME_MOTE2PC_SNIFFED_PACKET:
+    printf("mote2pc sniffed\n");
+    break;
+#if 0
+  case SERFRAME_PC2MOTE_SETDAGROOT:
+    printf("pc2mote setdagroot\n");
+    break;
+  case SERFRAME_PC2MOTE_DATA:
+    printf("pc2mote data\n");
+    break;
+  case SERFRAME_PC2MOTE_TRIGGERSERIALECHO:
+    printf("pc2mote triggerserialecho\n");
+    break;
+  case SERFRAME_PC2MOTE_COMMAND_GD:
+    printf("pc2mote GD\n");
+    break;
+#endif
+  case SERFRAME_ACTION_YES:
+    printf("action yes\n");
+    break;
+  case SERFRAME_ACTION_NO:
+    printf("action no\n");
+    break;
+  case SERFRAME_ACTION_TOGGLE:
+    printf("action toggle\n");
+    break;
+  default:
+    printf("invalid comment: %02x\n", inBuf[0]);
+    break;
+  }
+}
+
+void process_byte(unsigned char byte)
+{
+  static enum inputstate currentstate;
+  static int  inPos;
+  static char inBuf[2048];
+
+  switch(currentstate) {
+  case START:
+    switch(byte) {
+    case HDLC_FLAG:
+      inPos = 0;
+      currentstate = PROCESSING;
+      break;
+
+#if 0
+    default:
+      /* eat byte, continue */
+      break;
+#endif
+    }
+    break;
+
+  case PROCESSING:
+    switch(byte) {
+    case HDLC_FLAG:
+      /* end of packet */
+      currentstate = ENDFRAME;
+      break;
+
+    case HDLC_ESCAPE:
+      currentstate = ESCAPED;
+      break;
+
+    default:
+      inBuf[inPos++] = byte;
+      break;
+    }
+    break;
+
+  case ESCAPED:
+    switch(byte) {
+    case HDLC_FLAG_ESCAPED:
+      inBuf[inPos++] = HDLC_FLAG;
+      break;
+
+    case HDLC_ESCAPE_ESCAPED:
+      inBuf[inPos++] = HDLC_ESCAPE;
+      break;
+
+      /* should not happen, unless bytes get lost */
+    case HDLC_FLAG:
+      currentstate = ENDFRAME;
+
+    default:
+      inBuf[inPos++] = byte;
+      break;
+    }
+    currentstate = PROCESSING;
+    break;
+
+  case ENDFRAME:
+    break;
+  }
+
+  if(currentstate == ENDFRAME) {
+    unsigned int i;
+    unsigned short crc = HDLC_CRCINIT;
+
+    /* checkout the CRC, and if it is okay, pass frame upwards */
+    for(i=0; i < inPos; i++) {
+      unsigned char b = inBuf[i];
+      crc = (crc>>8)^FCS16TAB[((crc^(b)) & 0xff)];
+    }
+    if(crc == HDLC_CRCGOOD) {
+      /* good crc! */
+      inPos -= 2;
+      process_input(inBuf, inPos);
+    } else {
+      fprintf(stderr, "bad CRC: %02x vs %02x\n", crc, HDLC_CRCGOOD);
+    }
+
+    currentstate = START;
+  }
+}
 
 void read_from_serial(int motefd)
 {
-  unsigned inbuf[2];
+  unsigned char inbuf[2];
   static int count;
 
   if(read(motefd, inbuf, 1) > 0) {
-    printf("%02x ", inbuf[0]);
-    count++;
-    if((count & 0x0f) == 0) {
-      printf("\n");
-    }
+    process_byte(inbuf[0]);
   }
 }
 
@@ -139,4 +397,7 @@ main(int argc, char **argv)
       }
     }
   }
+
+  exit(0);
 }
+
