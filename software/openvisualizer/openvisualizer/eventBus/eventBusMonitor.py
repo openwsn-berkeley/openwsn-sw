4123c631b99c77546dea4f6b230b9654418743fd
changtengfei <tengfei.chang@gmail.com>
2017-02-03 11:36:30 +0100

    SW-273. enable Wireshark debug by default.

--- software/openvisualizer/openvisualizer/eventBus/eventBusMonitor.py
+++ software/openvisualizer/openvisualizer/eventBus/eventBusMonitor.py
@@ -30,7 +30,7 @@ class eventBusMonitor(object):
         # local variables
         self.dataLock                  = threading.Lock()
         self.stats                     = {}
-        self.wiresharkDebugEnabled     = False
+        self.wiresharkDebugEnabled     = True
         self.dagRootEui64              = [0x00]*8
         self.simMode                   = False
         
